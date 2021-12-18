ef948b2afd2c09cad3ee1fef5e5eae0379a25bd3
Ryan Sise <kerneltux0@gmail.com>
2021-12-18 10:22:23 -0700

    First few lessons

---
+++ UdemyTutorial/src/Strings.java
@@ -0,0 +1,13 @@
+
+public class Strings {
+	
+	public static void main(String[] args) {
+		String name = "Keith";
+		int age = 80;
+		String instrument = "guitar";
+		String activity = "warp";
+		
+		System.out.println("Hello, I am " + name + ". I am " + age + " years old, and I " + activity + " the " + instrument);
+	}
+
+}
---
+++ UdemyTutorial/src/Variables.java
@@ -0,0 +1,14 @@
+
+public class Variables {
+	
+	public static void main(String[] args) {
+		int something = 13;
+		double pie = 3.14159238;
+		float digits = 7.8f;
+		
+		System.out.println(something);
+		System.out.println(pie);
+		System.out.println(digits);
+	}
+
+}
